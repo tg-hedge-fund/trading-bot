3acd0a63e3a7ca4c099ae51f30a92d891096b87c
prathamesh <thundergod1180@gmail.com>
2026-03-01 12:27:39 +0530

    logger update

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ threads = []
 def run_instrument_and_token_schedule():
     try:
         if config.get("instrument_and_eq_schedule"):
-            logger.info("Running save instrument job...")
             schedule.every().sunday.do(scheduled_jobs_instrument, "IDX")
             run_job_every_mon_fri("09:15", scheduled_jobs_instrument, "EQ")
         run_job_every_mon_fri("07:00", generate_token_every_morning_mtof)
--- utils/jobs.py
+++ utils/jobs.py
@@ -3,6 +3,7 @@ import schedule
 from trade_utils.save_instruments import save_instrument_eq, save_instrument_idx
 from utils.discord_bot import send_message_via_discord_bot
 from utils.token_generator import generate_token
+from utils.utils import logger
 
 
 def run_job_every_mon_fri(time, fn, *args):
@@ -13,6 +14,7 @@ def run_job_every_mon_fri(time, fn, *args):
     schedule.every().friday.at(time).do(fn, *args)
 
 def scheduled_jobs_instrument(run_arg):
+    logger.info("Running save instrument job...")
     if (run_arg == "EQ"):
         save_instrument_eq()
     elif (run_arg == "IDX"):
